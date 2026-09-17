2f9fee9e152cbebf28fb7fc2543c43e26c0cefb4
ajamack <156970326+ajamack@users.noreply.github.com>
2026-09-17 14:19:36 -0400

    Add app.py to repository

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    print("Senior Project Developer Profile")
+    print()
+    print("Name: Aja")
+    print("Major: Computer Science")
+    print("Technology Interest: Artificial Intelligence in DoD realm")
+    print("Skill Goal: Project development in a team setting")
+
+if __name__ == "__main__":
+    main()
